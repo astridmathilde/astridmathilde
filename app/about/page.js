98481777af672c1c5df94c1d1918f142d5a12984
astridmathilde <heia@astridmathilde.no>
2025-02-23 00:14:06 +0100

    temp

---
+++ app/about/page.js
@@ -0,0 +1,67 @@
+import { siteTitle } from "../layout";
+import Link from "next/link";
+import style from "../../assets/scss/components/skills.module.scss";
+import utils from "../../assets/scss/utils.module.scss";
+import BlockIcon from "../../components/icon";
+import BlockRow from "../../components/row";
+import BlockColumn from "../../components/column";
+import BlockResumee from "../../components/resumee";
+import BlockImage from "../../components/image";
+import { experience, education, skills } from "../../data/resumee";
+
+import portrait from "../../assets/img/about/portrait-astrid-boberg.jpg";
+
+const pageTitle = 'About me';
+
+export const metadata = {
+  metadataBase: new URL('https://astridmathilde.no/about'),
+  title: pageTitle + ' – ' + siteTitle,
+}
+
+export default function Resumee() {
+  return (
+    <>
+    <BlockRow align="center">
+    <BlockColumn>
+    <h2 class={utils.screen_reader_text}>About me</h2>
+    <p>I was born in April 1998 (currently 26 <BlockIcon>🎉</BlockIcon>) and I have 6 years experience with digital design and coding after working for design agencies in Harstad, Tromsø and Oslo. In August 2021, I started doing a five year integrated master in design at <a href="https://aho.no" target="_blank" rel="external">The Oslo School of Architecture and Design</a>.</p>
+    </BlockColumn>
+    <BlockColumn>
+    <BlockImage url={portrait} alt="Astrid Mathilde Boberg" width="600" height="600" sizes="(min-width: 670px) 70vw, 100vw" priority />
+    </BlockColumn>
+    </BlockRow>
+    
+    <h3>Work experience <BlockIcon>👩🏻‍💻</BlockIcon></h3>
+    {experience.map((experience) => (
+      <BlockResumee key={experience.id} position={experience.position} locationUrl={experience.location.url} locationName={experience.location.name} description={experience.description} time={experience.time} />
+    ))}
+    
+    <h3>Education <BlockIcon>👩🏻‍🎓</BlockIcon></h3>
+    {education.map((education) => (
+      <BlockResumee key={education.id} position={education.grade} locationUrl={education.location.url} locationName={education.location.name} description={education.description} time={education.time} />
+    ))}
+    
+    
+    <div className={style.container}>
+    <BlockRow>
+    {skills.map((skills) => (
+      <BlockColumn key={skills.title}>
+      <h3>{skills.title}</h3>
+      <ul>
+      {skills.keywords.sort().map((keyword) => (
+        <li key={keyword}>{keyword}</li>
+      ))}
+      </ul>
+      </BlockColumn>
+    ))}
+    </BlockRow>
+    </div>
+    
+    <h2>Contact <BlockIcon>🤝</BlockIcon></h2>
+    <p>My email is <a href="mailto:heia@astridmathilde.no?subject=Heia!">heia@astridmathilde.no</a>, but you can also write to me on <a href="https://instagram.com/astridmathildeb" target="_blank" rel="nofollow noreferrer">Instagram</a> or add me on <a href="https://www.linkedin.com/in/astridmathilde/" target="_blank" rel="nofollow noreferrer">LinkedIn</a>!</p>
+    
+    <h2>Further discovery <BlockIcon>🔎</BlockIcon></h2>
+    <p>You can look at <Link href="/work/mensn">one of my projects</Link>, or see <Link href="/colophon">how I have reduced the carbon emissions on this website.</Link></p>
+    </>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -3,7 +3,33 @@ import BlockIcon from "../components/icon";
 export default function Index() {
   return (
     <>
-    <BlockIcon>☁️☁️☁️</BlockIcon>
+    <BlockDisplay>Heia! <BlockIcon>👋</BlockIcon> I am a designer and developer from Northern Norway <BlockIcon>🏔️</BlockIcon> with a passion for sustainability. Currently I am doing a master's degree in design at <a href="https://aho.no/en" target="_blank" rel="external">The Oslo School of Architecture and Design</a>, while leading the product development at <a href="https://www.gemz.fashion/" target="_blank" rel="external">Gemz</a> 💃</BlockDisplay>
+    
+    
+    <h2 className={utils.screen_reader_text}>This is some of my projects 👇</h2>
+    
+    <div className={style.projects}>
+    {projects.toReversed().map((project) => (
+      <BlockProjects id={project.id} slug={project.slug} title={project.title} category={project.category} location={project.location.name} thumbnail={project.thumbnail} priority="true" sizes="(min-width: 670px) 50vw, 100vw" />
+    ))}
+    </div>
+    
+    <h2>Recent work experience <BlockIcon>👩🏻‍💻</BlockIcon></h2>
+    {experience.slice(0, 3).map((experience) => (
+      <BlockResumee id={experience.id} position={experience.position} locationUrl={experience.location.url} locationName={experience.location.name} description={experience.description} time={experience.time} />
+    ))}
+    
+    <h2>Life lately <BlockIcon>✨</BlockIcon></h2>
+    <p>Continuing on my third year at <a href="https://aho.no/en" target="_blank" rel="external">The Oslo School of Architecture and Design</a> after having a break. In December I joined the startup <a href="https://www.gemz.fashion/" target="_blank" rel="external">Gemz</a> as their head of product and CTO, currently working on the MVP. I am trying to find a good way to balance this with my school projects. Otherwise I spend my days doing yoga, reading and listening to music. Also, I have started sharing my everyday observations in my own digital space called <a href="https://astrid.observer" rel="external" target="_blank"><em>blikkjournal</em></a>, a journal of things I have seen.</p>
+    
+    <p className={utils.small}>Last updated on February 9th, 2025. Inspired by <a href="https://nownownow.com/about" target="_blank" rel="external">Now</a>.</p>
+    
+    
+    <h2>Contact <BlockIcon>🤝</BlockIcon></h2>
+    <p>My email is <a href="mailto:heia@astridmathilde.no?subject=Heia!">heia@astridmathilde.no</a>, but you can also write to me on <a href="https://instagram.com/astridmathildeb" target="_blank" rel="nofollow noreferrer">Instagram</a> or <a href="https://www.linkedin.com/in/astridmathilde/" target="_blank" rel="nofollow noreferrer">add me on LinkedIn</a>!</p>
+    
+    <h2>Further discovery <BlockIcon>🔎</BlockIcon></h2>
+    <p>You can <Link href="/about">learn more about me</Link> or find out <Link href="/colophon">why I am not using generative AI</Link>.</p>
     </>
   );
 }
