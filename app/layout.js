d10d93e4e62f24f2f27782bfcdf696c79082b7cb
astridmathilde <heia@astridmathilde.no>
2025-04-12 23:20:34 +0200

    changed site title (again)

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,7 @@ const inter = localFont({
   ]
 })
 
-export const siteTitle = 'Astrid Boberg';
+export const siteTitle = 'Astrid Mathilde Boberg';
 
 export default function Layout({ children }) {
   return (
@@ -31,7 +31,7 @@ export default function Layout({ children }) {
     <Wrapper>
     <Providers>
     <header id={styles.header} className={"text-medium"}>
-    <h1 className={styles.site_title}><a href="/">{siteTitle}</a></h1>
+    <h1 className={styles.site_title}><a href="/">Astrid Mathilde</a></h1>
     <nav className={styles.navigation}>
     <ul>
     <li key="work"><Link href="/work">Work</Link></li>
