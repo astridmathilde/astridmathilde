59a6d871e83360912ebdcffa963e9cc81d61a960
astridmathilde <heia@astridmathilde.no>
2025-08-22 22:27:59 +0200

    finally updated my now page

--- app/now/page.js
+++ app/now/page.js
@@ -7,32 +7,32 @@ const pageTitle = 'Now';
 
 export const metadata = {
   metadataBase: new URL('https://astridmathilde.no/now'),
-  title: pageTitle + ' – ' + siteTitle,
-  description: 'Continuing on my third year at the Oslo School of Architecture and Design after having a break. My life currently revolves around a project I am doing in collaboration with Norwegian Red Cross, which I will be working with until the end of May. I am sharing my everyday observations in my own digital space called blikkjournal.'
+  title: pageTitle + ' – ' + siteTitle
 }
 
 export default function Colophon() {
   return (
     <>
     <h2>What I am doing now <BlockIcon>👀</BlockIcon></h2>
-    <p>Continuing on my third year at the <a href="https://aho.no/en" target="_blank" rel="external">Oslo School of Architecture and Design</a> after having a break. My life currently revolves around a project I am doing in collaboration with <a href="https://www.rodekors.no/en/" rel="external" target="_blank">Norwegian Red Cross</a>, which I will be working with until the end of May. I share my everyday observations in <a href="https://astrid.observer" rel="external" target="_blank">my own digital space</a>, and I enjoy listening to music and reading books.</p>
+    <p>Just started my fourth year at the <a href="https://aho.no/en" target="_blank" rel="external">Oslo School of Architecture and Design</a>, it is weird how fast time flies. Also, I switched to a dumb phone (Punkt MP01) and it is going well so far. Found a old digital camera too, so that I can continue posting stuff in my <a href="https://blikk.directory" target="_blank" rel="external">blikkjournal</a>. My personal projects have been neglected this summer due to an internship and a freelance project, so I am looking forward to continue working on my own things while getting back into my routines. </p>
     
-    <p className={utils.small}>Last updated on April 12th, 2025. Inspired by <a href="https://nownownow.com/about" target="_blank" rel="external">Now</a>.</p>
+    <p className={utils.small}>Last updated on August 22th, 2025. Inspired by <a href="https://nownownow.com/about" target="_blank" rel="external">Now</a>.</p>
     
     
     <h3>Currently reading <BlockIcon>📚</BlockIcon></h3>
     <ul>
-    <li><em>Design med ord</em>, Susanne Egset</li>
+    <li><em>The Design of Everyday Things</em>, Don Norman</li>
     <li><em>The Myth of Sisyphus</em>, Albert Camus</li>
-    <li><em>Transit</em>, Rachel Cusk</li>
     <li><em>The Creative Act</em>, Rick Rubin</li>
+    <li><em>Design after Capitalism</em>, Matthew Wizinsky</li>
+    <li><em>The Body Keeps The Score</em>, Bessel Van Der Kolk</li>
     </ul>
     
     <h3>Music on repeat <BlockIcon>🎧</BlockIcon></h3>
     <ul>
-    <li>My playlist <a href="https://open.spotify.com/playlist/2S2SPH0K6QlOlJLwJEG9oR?si=33327c5ba3914c74" rel="external norefferer" target="_blank">CORE</a></li>
-    <li><a href="https://open.spotify.com/track/6rYgEHuyhO064Lgj48qlIe?si=e3a2ff74732b4597" rel="external norefferer" target="_blank">I'll call you mine</a> by girlinred</li>
-    <li>The album <a href="https://open.spotify.com/album/6PrbSnRK23zsgmgoMeBIoj?si=X1zWkxHCSmOd_wv9eXezLw" target="_blank"><em>ROTATIONS</em></a> by The Album Leaf</li>
+    <li>My playlist <a href="https://open.spotify.com/playlist/4kL8Rq4ZXppyp6RGCU7vB1?si=50321d2e062f42bf" rel="external norefferer" target="_blank"><em>slow</em></a></li>
+    <li>The song <a href="https://open.spotify.com/track/7hGCCQkdyF1MX6uk339uBS?si=d756a982721b4f13" rel="external norefferer" target="_blank">"What Was That"</a> by Lorde</li>
+    <li>The album <a href="https://open.spotify.com/album/69p42ovQdwmYPLUsAQpeNn?si=fAA2pGc_R3GMKosIs33WLg" target="_blank"><em>if i could make it go quiet</em></a> by girlinred</li>
     
     </ul>
     
--- app/page.js
+++ app/page.js
@@ -29,10 +29,10 @@ export default function Index() {
     </div>
     
     <h2>What I am doing now <BlockIcon>👀</BlockIcon></h2>
-    <p>Continuing on my third year at the <a href="https://aho.no/en" target="_blank" rel="external">Oslo School of Architecture and Design</a> after having a break. My life currently revolves around a project I am doing in collaboration with <a href="https://www.rodekors.no/en/" rel="external" target="_blank">Norwegian Red Cross</a>, which I will be working with until the end of May. I share my everyday observations in <a href="https://astrid.observer" rel="external" target="_blank">my own digital space</a>, and I enjoy listening to music and reading books, you can <Link href="now">check it out on my Now-page</Link>.</p>
+    <p>Just started my fourth year at the <a href="https://aho.no/en" target="_blank" rel="external">Oslo School of Architecture and Design</a> – it is weird how fast time flies. Also, I switched to a dumb phone (Punkt MP01) and it is going well so far. Found a old digital camera too, so that I can continue posting photos in my <a href="https://blikk.directory" target="_blank" rel="external">blikkjournal</a>. My personal projects have been neglected this summer due to an internship and a freelance project, so I am looking forward to continue working on my own things while getting back into my routines.</p>
     
-    <p className={utils.small}>Last updated on April 12th, 2025. Inspired by <a href="https://nownownow.com/about" target="_blank" rel="external">Now</a>.</p>
-    
+    <p className={utils.small}>Last updated on August 22th, 2025. Inspired by <a href="https://nownownow.com/about" target="_blank" rel="external">Now</a>.</p>
+
     <h2>Recent work experience <BlockIcon>👩🏻‍💻</BlockIcon></h2>
     {experience.slice(0, 3).map((experience) => (
       <BlockResumee id={experience.id} position={experience.position} locationUrl={experience.location.url} locationName={experience.location.name} description={experience.description} time={experience.time} />
@@ -42,7 +42,7 @@ export default function Index() {
     <p>My email is <a href="mailto:heia@astridmathilde.no?subject=Heia!">heia@astridmathilde.no</a>, but you can also <a href="https://www.linkedin.com/in/astridmathilde/" target="_blank" rel="nofollow noreferrer">add me on LinkedIn</a>!</p>
     
     <h2>Further discovery <BlockIcon>🔎</BlockIcon></h2>
-    <p>You can <Link href="/about">learn more about me</Link>, see <Link href="/now">what I am currently listening to</Link>, or find out <Link href="/colophon">why I am not using generative AI</Link>.</p>
+    <p>You can <Link href="/about">learn more about me</Link>, see <Link href="/now">what I am currently reading</Link>, or find out <Link href="/colophon">why I am not using generative AI</Link>.</p>
     </div>
   );
 }
