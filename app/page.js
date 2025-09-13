81c7b4f0c4e26469b321f1501153fc83486ce14c
astridmathilde <heia@astridmathilde.no>
2025-09-13 13:34:50 +0200

    just me and another typo

--- app/page.js
+++ app/page.js
@@ -12,13 +12,13 @@ import { experience } from "../data/resumee";
 export const metadata = {
   metadataBase: new URL('https://astridmathilde.no'),
   title: siteTitle + " – Designer & developer",
-  description: 'Heia! I am a digital product designer and self-taught frontend developer from Northern Norway with a background in graphic design. Currently I am living in Oslo doing an MA in design, while doing freelance work on websites and applications. Also, I enjoy writing things, taking photos of stuff and spending time in nature.'
+  description: "Heia! I'm a digital product designer and self-taught frontend developer from Northern Norway with a background in graphic design. Currently I'm living in Oslo doing a master's degree in design. I'm passionate about reducing the carbon impact of the digital products we create, and I also enjoy writing things, taking photos of stuff and spending time in nature."
 }
 
 export default function Index() {
   return (
     <div className={style.content}>
-    <BlockDisplay location="home">Heia! I'm a digital product designer and self-taught frontend developer from Northern Norway <BlockIcon>🏔️</BlockIcon> with a background in graphic design. Currently I'm living in Oslo doing a master's degree in design. I'm passionate about reducing the carbon impact of the digital products we create, and I also enjoy writing things, <a href="https://blikk.directory" rel="external" title="my library of observations" target="_blank">taking photos of stuff</a>  and spending time in nature.</BlockDisplay>
+    <BlockDisplay location="home">Heia! I'm a digital product designer and self-taught frontend developer from Northern Norway <BlockIcon>🏔️</BlockIcon> with a background in graphic design. Currently I'm living in Oslo doing a master's degree in design. I'm passionate about reducing the climate impact of the digital products we create, and I also enjoy writing things, <a href="https://blikk.directory" rel="external" title="my library of observations" target="_blank">taking photos of stuff</a>  and spending time in nature.</BlockDisplay>
     
     <h2>Selected projects <BlockIcon>👇</BlockIcon></h2>
     
