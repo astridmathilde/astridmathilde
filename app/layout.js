fc4e257612675b210fcb3fb88b1c1c59962689ab
astridmathilde <heia@astridmathilde.no>
2025-09-19 11:37:59 +0200

    changed the order of the links in footer

--- app/layout.js
+++ app/layout.js
@@ -47,8 +47,8 @@ export default function Layout({ children }) {
     <address rel="author">© Astrid Mathilde Boberg</address>
     <nav className={styles.navigation}>
     <ul>
-    <li key="now"><Link href="/now">Now</Link></li>
     <li key="colophon"><Link href="/colophon">Colophon</Link></li>
+    <li key="now"><Link href="/now">Now</Link></li>
     </ul>
     </nav>
     </footer>
