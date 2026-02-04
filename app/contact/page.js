bf5a47e0c7236f40913b05ef6a746a29adfa872b
astridmathilde <heia@astridmathilde.no>
2026-02-04 15:46:43 +0100

    updated contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -12,14 +12,14 @@ export default function Now() {
   return (
     <>
     <h2 className={utils.screen_reader_text}>Contact</h2>
-    <p>My email is <a href="mailto:heia@astridmathilde.no?subject=Heia!">heia@astridmathilde.no</a>, but you can also <a href="https://www.linkedin.com/in/astridmathilde/" target="_blank" rel="nofollow noreferrer">add me on LinkedIn</a>!</p>
+    <p>My email is heia@astridmathilde.no, but you can also <a href="https://www.linkedin.com/in/astridmathilde/" target="_blank" rel="nofollow noreferrer">add me on LinkedIn</a>!</p>
     
     <h3>Links</h3>
     <nav>
     <ul>
-    <li><a href="https://blikk.directory" rel="external" target="_bank">blikk.directory</a></li>
+    <li><a href="https://blikk.directory" rel="external" target="_blank">blikk.directory</a></li>
     <li><a href="https://www.are.na/astrid-mathilde-boberg" rel="external" target="_blank">Are.na</a></li>
-    <li><a href="https://github.com/astridmathilde/astridmathilde">Github</a></li>
+    <li><a href="https://github.com/astridmathilde" rel="external" target="_blank">Github</a></li>
     <li><a href="https://www.linkedin.com/in/astridmathilde/" target="_blank" rel="nofollow noreferrer">LinkedIn</a></li>
     </ul>
     </nav>    
