622f42dc57e33f68943d8fb8940c9b8d3a5cfe4c
astridmathilde <heia@astridmathilde.no>
2026-02-04 15:45:16 +0100

    updated now page

--- app/now/page.js
+++ app/now/page.js
@@ -12,9 +12,9 @@ export default function Now() {
   return (
     <>
     <h2 className={utils.screen_reader_text}>Current status</h2>
-    <p>Currently on my fourth year at the <a href="https://aho.no/en" target="_blank" rel="external">Oslo School of Architecture and Design</a>. I am focusing on my personal projects, including the website <a href="https://blikk.directory" rel="external" target="_blank">blikkjournal</a> and the one you are browsing right now, figuring out a better way to showcase my work. Otherwise I still have a minimalistic phone – my beloved Punkt MP02 – and when I went to Northern Norway for Christmas, I found my old iPod Nano in the basement of my dad's house, and that made me very happy.</p>
+    <p>Currently on my fourth year at the <a href="https://aho.no/en" target="_blank" rel="external">Oslo School of Architecture and Design</a>. Otherwise I am focusing on my personal projects, including the development of <a href="https://blikk.directory" rel="external" target="_blank">Blikkjournal</a> (my personal archive of moments and everyday observations) and the website you are browsing right now. After this I am planning to work on some previously neglected writings, and maybe get back into bookbinding.</p>
     
-    <p className={utils.small}>Last updated on January 18th, 2026. Inspired by <a href="https://nownownow.com/about" target="_blank" rel="external">Now</a>.</p>
+    <p className={utils.small}>Last updated on February 4th, 2026. Inspired by <a href="https://nownownow.com/about" target="_blank" rel="external">Now</a>.</p>
 
     <h3>Currently reading</h3>
     <ul>
@@ -23,8 +23,9 @@ export default function Now() {
 
     <h3>Music on repeat</h3>
     <ul>
-      <li>The album <em>carpool etiquette</em> by rutherford</li>
-      <li>The song "How Soon is Now?" by The Smiths</li>
+      <li>The album <em>Secret Life</em> by Fred again.. & Bibio</li>
+      <li>The song "someday" by rutherford</li>
+      <li>The band Arctic Monkeys</li>
     </ul>
     </>
   )
