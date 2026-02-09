fbf6231025b0351731b9a6c4f8fabbc99aae3c9e
astridmathilde <heia@astridmathilde.no>
2026-02-09 19:12:06 +0100

    reduced number of links

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Index() {
     <>
     <h2 className={utils.screen_reader_text}>Introduction</h2>
     <div className={style.content}>
-    <p>Heia! I am a designer and developer from Northern Norway, <Link href="now" title="my current status">currently doing</Link> a master’s in interaction design in Oslo. With a background in graphic design, web design and development, I now specialise in designing digital products. I also enjoy writing things, <a href="https://blikk.directory" rel="external" title="my library of observations" target="_blank">taking photos of stuff</a>, and I have an interest for digital minimalism and <Link href="colophon" title="this site's climate footprint">web sustainability</Link>.</p>
+    <p>Heia! I am a designer and developer from Northern Norway, currently doing a master’s in interaction design in Oslo. With a background in graphic design, web design and development, I now specialise in designing digital products. I also enjoy writing things, <a href="https://blikk.directory" rel="external" title="my library of observations" target="_blank">taking photos of stuff</a>, and I have an interest for digital minimalism and <Link href="colophon" title="this site's climate footprint">web sustainability</Link>.</p>
     
     <h2>Index</h2>
     <nav>
