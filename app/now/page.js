9497c66502dbe1d8d862c248e142de7dc1dec239
astridmathilde <heia@astridmathilde.no>
2026-02-09 20:37:48 +0100

    updated my now-page

--- app/now/page.js
+++ app/now/page.js
@@ -13,20 +13,20 @@ export default function Now() {
   return (
     <>
     <h2 className={utils.screen_reader_text}>Current status</h2>
-    <p>Currently on my fourth year at the <a href="https://aho.no/en" target="_blank" rel="external">Oslo School of Architecture and Design</a>. Otherwise I am focusing on my personal projects, including the development of <a href="https://blikk.directory" rel="external" target="_blank">Blikkjournal</a> (my personal archive of moments and everyday observations) and the website you are browsing right now. After this I am planning to work on some previously neglected writings, and maybe get back into bookbinding.</p>
+    <p>Currently on my fourth year at the <a href="https://aho.no/en" target="_blank" rel="external">Oslo School of Architecture and Design</a>. My life is calm and quiet right now, focusing on school assignments and personal projects, including the development of <a href="https://blikk.directory" rel="external" target="_blank">Blikkjournal</a> (my personal archive of moments and everyday observations) and the website you are browsing right now. Until I figure out a better way to structure this site and showcase my work, everything will be more simple and calm than usual, like my life lately :)</p>
     
-    <p className={utils.small}>Last updated on February 4th, 2026. Inspired by <a href="https://nownownow.com/about" target="_blank" rel="external">Now</a>.</p>
+    <p className={utils.small}>Last updated on February 9th, 2026. Inspired by <a href="https://nownownow.com/about" target="_blank" rel="external">Now</a>.</p>
+    
+    <h3>Music on repeat</h3>
+    <ul>
+    <li>Everything by rutherford</li>
+    <li>The album <em>AM</em> by Arctic Monkeys</li>
+    <li>The song "I saw you" by Fred again.. & Bibio</li>
+    </ul>
     
     <h3>Currently reading</h3>
     <ul>
     <li><em>The Creative Act</em>, Rick Rubin</li>
-    </ul>
-    
-    <h3>Music on repeat</h3>
-    <ul>
-    <li>The album <em>Secret Life</em> by Fred again.. & Bibio</li>
-    <li>The song "someday" by rutherford</li>
-    <li>The band Arctic Monkeys</li>
     </ul>
     </>
   )
