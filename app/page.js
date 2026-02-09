160a7de145c7c4f7c3766477d86936208ebf0d43
astridmathilde <heia@astridmathilde.no>
2026-02-09 19:11:48 +0100

    removed unused

--- app/page.js
+++ app/page.js
@@ -15,14 +15,13 @@ export default function Index() {
     <>
     <h2 className={utils.screen_reader_text}>Introduction</h2>
     <div className={style.content}>
-    <p>Heia! I am a designer and developer from Northern Norway, <Link href="now" title="my current status">currently doing</Link> a master’s in interaction design in Oslo. With a background in graphic design, web design and development, I now specialise in designing digital products. I also enjoy <Link href="notes" rel="my notes">writing things</Link>, <a href="https://blikk.directory" rel="external" title="my library of observations" target="_blank">taking photos of stuff</a>, and I have an interest for digital minimalism and <Link href="colophon" title="this site's climate footprint">web sustainability</Link>.</p>
+    <p>Heia! I am a designer and developer from Northern Norway, <Link href="now" title="my current status">currently doing</Link> a master’s in interaction design in Oslo. With a background in graphic design, web design and development, I now specialise in designing digital products. I also enjoy writing things, <a href="https://blikk.directory" rel="external" title="my library of observations" target="_blank">taking photos of stuff</a>, and I have an interest for digital minimalism and <Link href="colophon" title="this site's climate footprint">web sustainability</Link>.</p>
     
     <h2>Index</h2>
     <nav>
     <ul>
     <li><Link href="about">About</Link></li>
     <li><Link href="now">Now</Link></li>
-    <li><Link href="notes">Notes</Link></li>
     <li><Link href="resumee">Resumee</Link></li>
     <li><Link href="contact">Contact</Link></li>
     </ul>
