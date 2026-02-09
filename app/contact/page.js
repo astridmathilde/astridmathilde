c50901e97c04f3c00799444829131ac07be3d50c
astridmathilde <heia@astridmathilde.no>
2026-02-09 16:51:30 +0100

    a face to the name

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,5 +1,8 @@
 import { siteTitle } from "../layout";
-import utils from "../assets/scss/utils.module.scss"
+import utils from "../assets/scss/utils.module.scss";
+import BlockRow from "../components/row";
+import BlockColumn from "../components/column";
+import BlockImage from "../components/image";
 
 const pageTitle = 'Contact';
 
@@ -8,9 +11,11 @@ export const metadata = {
   title: pageTitle + ' – ' + siteTitle
 }
 
-export default function Now() {
+import img from "../assets/img/about/astridmathilde.jpeg";
+export default function Contact() {
   return (
-    <>
+    <BlockRow>
+    <BlockColumn>
     <h2 className={utils.screen_reader_text}>Contact</h2>
     <p>My email is heia@astridmathilde.no, but you can also <a href="https://www.linkedin.com/in/astridmathilde/" target="_blank" rel="nofollow noreferrer">add me on LinkedIn</a>!</p>
     
@@ -22,7 +27,12 @@ export default function Now() {
     <li><a href="https://github.com/astridmathilde" rel="external" target="_blank">Github</a></li>
     <li><a href="https://www.linkedin.com/in/astridmathilde/" target="_blank" rel="nofollow noreferrer">LinkedIn</a></li>
     </ul>
-    </nav>    
-    </>
+    </nav>
+    </BlockColumn>
+
+    <BlockColumn width="25">
+      <BlockImage url={img} alt="Astrid Mathilde Boberg" />
+    </BlockColumn>
+    </BlockRow>
   )
 }
