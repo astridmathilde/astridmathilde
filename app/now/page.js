1b8cca659d0da705d5597edcc852c6968334aa77
astridmathilde <heia@astridmathilde.no>
2026-03-16 11:42:21 +0100

    status update 📆

--- app/now/page.js
+++ app/now/page.js
@@ -13,20 +13,20 @@ export default function Now() {
   return (
     <>
     <h2 className={utils.screen_reader_text}>Current status</h2>
-    <p>Currently on my fourth year at the <a href="https://aho.no/en" target="_blank" rel="external">Oslo School of Architecture and Design</a>. My life is calm and quiet right now. I am focusing on school assignments and personal projects, including the development of <a href="https://blikk.directory" rel="external" target="_blank">Blikkjournal</a> (my personal archive of moments and everyday observations) and the website you are browsing right now. Until I figure out a better way to structure this site and showcase my work, everything will be more simple and calm than usual, like my life lately :)</p>
+    <p>Currently on my fourth year at the <a href="https://aho.no/en" target="_blank" rel="external">Oslo School of Architecture and Design</a>. I recently lost someone important to me, but I think everything will be okay in the end. I am focusing on school assignments and personal projects, including the development of <a href="https://blikk.directory" rel="external" target="_blank">Blikkjournal</a> (my personal archive of moments and everyday observations) and the website you are browsing right now, and I have finally figured out a good way to structure this site and showcase my work. Music is more important to me than ever, and I appreciate that my life is still calm and quiet despite everything, and that spring is just around the corner.</p>
     
-    <p className={utils.small}>Last updated on February 9th, 2026. Inspired by <a href="https://nownownow.com/about" target="_blank" rel="external">Now</a>.</p>
+    <p className={utils.small}>Last updated on March 16th, 2026. Inspired by <a href="https://nownownow.com/about" target="_blank" rel="external">Now</a>.</p>
     
     <h3>Music on repeat</h3>
     <ul>
-    <li>Everything by rutherford</li>
-    <li>The album <em>AM</em> by Arctic Monkeys</li>
-    <li>The song "I saw you" by Fred again.. & Bibio</li>
+    <li>The album <em>God's Hand</em> by Hot Sugar</li>
+    <li>The album <em>Born To Die</em> by Lana Del Ray</li>
     </ul>
     
     <h3>Currently reading</h3>
     <ul>
     <li><em>The Creative Act</em>, Rick Rubin</li>
+    <li><em>Harry Potter and the Prisoner of Azkaban</em>, J.K Rowling</li>
     </ul>
     </>
   )
--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import utils from "./assets/scss/utils.module.scss";
 export const metadata = {
   metadataBase: new URL('https://astridmathilde.no'),
   title: siteTitle + " – Designer & developer",
-  description: "Heia! I am a designer and developer from Northern Norway, currently doing a master’s in interaction design in Oslo. With a background in graphic design, web design and development, I now specialise in designing digital products. I also enjoy writing things, and I have an interest for digital minimalism and web sustainability."
+  description: "Heia! I am a designer and developer from Northern Norway, currently doing a master’s in design at The Oslo School of Architecture and Design. With a background in graphic design, web design and development, I now specialise in designing digital products. I also enjoy writing things, and I have an interest for digital minimalism and web sustainability."
   
 }
 
@@ -13,7 +13,7 @@ export default function Index() {
   return (
     <>
     <h2 className={utils.screen_reader_text}>Introduction</h2>
-    <p>Heia! I am a designer and developer from Northern Norway, currently doing a master’s in interaction design in Oslo. With a background in graphic design, web design and development, I now specialise in designing digital products. I also enjoy writing things, <a href="https://blikk.directory" rel="external" title="my library of observations" target="_blank">taking photos of stuff</a>, and I have an interest for digital minimalism and <Link href="colophon" title="this site's climate footprint">web sustainability</Link>.</p>
+    <p>Heia! I am a designer and developer from Northern Norway, currently doing a master’s in design at The Oslo School of Architecture and Design. With a background in graphic design, web design and development, I now specialise in designing digital products. I also enjoy writing things, <a href="https://blikk.directory" rel="external" title="my library of observations" target="_blank">taking photos of stuff</a>, and I have an interest for digital minimalism and <Link href="colophon" title="this site's climate footprint">web sustainability</Link>.</p>
     
     <h2>Index</h2>
     <nav>
