932e699186121b2e653a523868d4a1d46c51cf95
astridmathilde <heia@astridmathilde.no>
2026-04-18 18:44:59 +0200

    updatesss

--- app/now/page.js
+++ app/now/page.js
@@ -13,14 +13,15 @@ export default function Now() {
   return (
     <>
     <h2 className={utils.screen_reader_text}>Current status</h2>
-    <p>Currently on my fourth year at the <a href="https://aho.no/en" target="_blank" rel="external">Oslo School of Architecture and Design</a>. I recently lost someone important to me, but I think everything will be okay in the end. I am focusing on school assignments and personal projects, including the development of <a href="https://blikk.directory" rel="external" target="_blank">Blikkjournal</a> (my personal archive of moments and everyday observations) and the website you are browsing right now, and I have finally figured out a good way to structure this site and showcase my work. Music is more important to me than ever, and I appreciate that my life is still calm and quiet despite everything, and that spring is just around the corner.</p>
+    <p>On the final module of my fourth year at the <a href="https://aho.no/en" target="_blank" rel="external">Oslo School of Architecture and Design</a>, doing a project in collaboration with <a href="https://www.siemens.com/en-us/products/emobility/"  target="_blank" rel="external">Siemens eMobility</a>. I am not doing any freelance work this year, which is giving me lots of time to work on my personal projects, for instance the development of <a href="https://blikk.directory" rel="external" target="_blank">blikkjournal</a> (my personal archive of moments and everyday observations), a project I have now put on hold to focus on the development of this website and actually showcasing some of my work. Also, I have started getting into the concept of owning the music I listen to, a very slow process, but I guess that is a part of the fun.</p>
     
-    <p className={utils.small}>Last updated on March 16th, 2026. Inspired by <a href="https://nownownow.com/about" target="_blank" rel="external">Now</a>.</p>
+    <p className={utils.small}>Last updated on April 18th, 2026. Inspired by <a href="https://nownownow.com/about" target="_blank" rel="external">Now</a>.</p>
     
     <h3>Music on repeat</h3>
     <ul>
-    <li>The album <em>God's Hand</em> by Hot Sugar</li>
-    <li>The album <em>Born To Die</em> by Lana Del Ray</li>
+    <li>The album <em>The Campfire Headphase</em> by Boards of Canada</li>
+    <li>"Polynomial-C" by Aphex Twin</li>
+    <li>The album <em>Swim Slowly</em> by Meltt</li>
     </ul>
     
     <h3>Currently reading</h3>
--- app/page.js
+++ app/page.js
@@ -13,8 +13,8 @@ export default function Index() {
   return (
     <>
     <h2 className={utils.screen_reader_text}>Introduction</h2>
-    <p>Heia! I am a designer and developer from Northern Norway, currently doing a master’s in design at The Oslo School of Architecture and Design. With a background in graphic design, web design and development, I now specialise in designing digital products. I also enjoy writing things, <a href="https://blikk.directory" rel="external" title="my library of observations" target="_blank">taking photos of stuff</a>, and I have an interest for digital minimalism and <Link href="colophon" title="this site's climate footprint">web sustainability</Link>.</p>
-    
+    <p>Heia! I am a designer and developer from Northern Norway, currently doing a master's in design at AHO. With a background in graphic design, web design and development, I now specialise in designing digital products – and I am particularly interested in software design. I also enjoy writing things, <a href="https://blikk.directory" rel="external" title="my library of observations" target="_blank">taking photos of stuff</a>, and I have an interest for digital minimalism and <Link href="colophon" title="this site's climate footprint">web sustainability</Link>.</p>
+
     <h2>Index</h2>
     <nav>
     <ul>
