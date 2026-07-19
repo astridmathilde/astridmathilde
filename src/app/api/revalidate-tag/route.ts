b34a3bd398a215a148ee318df749f9ffd086f7d3
astrid mathilde <heia@astridmathilde.no>
2026-07-20 00:51:42 +0200

    caching stuff

---
+++ src/app/api/revalidate-tag/route.ts
@@ -0,0 +1,41 @@
+import { revalidateTag } from 'next/cache'
+import { type NextRequest, NextResponse } from 'next/server'
+import { parseBody } from 'next-sanity/webhook'
+
+type WebhookPayload = {
+  _type: string
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    if (!process.env.SANITY_REVALIDATE_SECRET) {
+      return new Response(
+        'Missing environment variable SANITY_REVALIDATE_SECRET',
+        { status: 500 }
+      )
+    }
+    const { isValidSignature, body } = await parseBody<WebhookPayload>(
+      req,
+      process.env.SANITY_REVALIDATE_SECRET,
+      true // wait for Content Lake propagation
+    )
+    if (!isValidSignature) {
+      return new Response(
+        JSON.stringify({ message: 'Invalid signature', isValidSignature, body }),
+        { status: 401 }
+      )
+    }
+    if (!body?._type) {
+      return new Response(
+        JSON.stringify({ message: 'Bad Request', body }),
+        { status: 400 }
+      )
+    }
+    revalidateTag(body._type)
+    return NextResponse.json({ body })
+  } catch (err: unknown) {
+    console.error(err)
+    const message = err instanceof Error ? err.message : 'Unknown error'
+    return new Response(message, { status: 500 })
+  }
+}
