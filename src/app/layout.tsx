7f8249b223ec60b1f31e1e733d67c74c087a1a5c
astrid mathilde <heia@astridmathilde.no>
2026-08-12 13:09:54 +0200

    added some kind privacy-first vegan cookie-free and gluten-free tracking 🕵🏻‍♀️

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "next-themes";
 import type { Metadata } from 'next';
+import Script from "next/script";
 import localFont from "next/font/local";
 import Wrapper from "./wrapper";
 
@@ -29,6 +30,7 @@ const inter = localFont({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+    <Script defer src="https://cloud.umami.is/script.js" data-website-id="20a9b5e4-0833-4041-a73b-b6d93474b420" />
     <body>
     <ThemeProvider>
     <Wrapper>
